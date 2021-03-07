d3fed7c9b221ed2651122d14c404ad18f9aa4f5c
kimyunsu <kimdia2000@gmail.com>
2021-03-07 14:32:42 +0900

    [개인프로젝트] 기본틀

---
+++ js/KimYS.js
@@ -0,0 +1,29 @@
+
+var test1 = function(li) {
+  var sections = document.getElementsByTagName("section");
+
+  //모든 세션 display:none
+  for(var i=0; i<sections.length; i++){
+    sections[i].style.display = "none";
+  }
+
+  //클릭한 값에 따라 세션 창 보여줌
+  if(li.innerHTML == "Main"){
+    content1.style.display = "block";
+  }
+  else if(li.innerHTML == "Introduction"){
+    content2.style.display = "block";
+  }
+  else if(li.innerHTML == "Interests"){
+    content3.style.display = "block";
+  }else if(li.innerHTML == "BluePrint"){
+    content4.style.display = "block";
+  }else{
+    content5.style.display = "block";
+  }
+
+}
+
+for (var i = 1; i <= 2500; i++) {
+  $(".container").append('<div class="box"></div>');
+}
