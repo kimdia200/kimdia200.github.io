f88796e54615c3ca480328302fce927ba148dbfa
kimyunsu <kimdia2000@gmail.com>
2021-03-09 21:47:46 +0900

    Introduction 세션 - 내용 DVI 애니메이션 및 버튼제어 구현

--- js/KimYS.js
+++ js/KimYS.js
@@ -16,16 +16,33 @@ var test1 = function(li) {
   }
   else if(li.innerHTML == "Interests"){
     content3.style.display = "block";
-  }else if(li.innerHTML == "BluePrint"){
+  }else if(li.innerHTML == "Roadmap"){
     content4.style.display = "block";
   }else{
     content5.style.display = "block";
   }
 }
 // Intro부분 클릭에 따라 실행됨
+//1 버튼 클릭시 첫번째 div만 보여줌(애니메이션 없음)
+var click_1 = function(){
+  introDiv_descript1.style.display='block'
+  introDiv_descript2.style.display='none'
+  introDiv_descript1.style['animation-name'] = 'none';
+  introDiv_descript2.style['animation-name'] = 'move';
+}
+//2 버튼 클릭시 두번째 div만 보여줌(애니메이션 없음)
+var click_2 = function(){
+  introDiv_descript1.style.display='none'
+  introDiv_descript2.style.display='block'
+  introDiv_descript1.style['animation-name'] = 'move';
+  introDiv_descript2.style['animation-name'] = 'none';
+}
 // re버튼 클릭시 창을 다시 띄워줘서 모든 효과가 다시 나오게함
 var click_re = function(){
+  introDiv_descript1.style.display='block'
+  introDiv_descript2.style.display='block'
+  introDiv_descript1.style['animation-name'] = 'descript1';
+  introDiv_descript2.style['animation-name'] = 'descript2';
   var str = content2.innerHTML;
   content2.innerHTML = str;
 }
-
